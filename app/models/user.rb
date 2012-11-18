7d1a297d994ffc6723e59d00defa475c98898f59
Maurice Mauser <maurice.mauser@gmail.com>
2012-11-18 12:51:15 +0100

    Created user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,23 @@
 class User < ActiveRecord::Base
-  attr_accessible :cv, :education, :email, :experience, :image, :location, :name, :provider
+  attr_accessible :uid, :cv, :education, :email, :experience, :image, :location, :name, :provider
+
+  def self.from_omniauth(auth)
+  	logger.debug "============================================"
+    logger.debug auth.to_yaml()
+    logger.debug auth["info"]["image"]
+  	where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
+  end
+
+  def self.create_from_omniauth(auth)
+  	create! do |user|
+  		user.provider = auth["provider"]
+  		user.uid = auth["uid"]
+  		# user.name = auth["info"]["nickname"]
+  		user.name = auth["info"]["name"]
+  		user.email = auth["info"]["email"]
+      user.image = auth["info"]["profile_image_url"]
+  	end
+  end
 end
+
+# https://dev.twitter.com/docs/platform-objects/users
